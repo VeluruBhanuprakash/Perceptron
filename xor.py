7ce5c9560a0d17df7f141f28205f647408bc15f5
bhanuprakash <vbhanuprakash.veluru@gmail.com>
2022-01-23 21:31:34 +0530

    logging added for XOR model

--- xor.py
+++ xor.py
@@ -1,11 +1,23 @@
 from utils.all_utils import prepare_data,save_plot
 import pandas as pd
 from utils.model import Perceptron
+import logging
+import os
 
+log_dir = "logs"
+gate = "XOR Gate"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(
+    filename=os.path.join("logs","running_logs.log"),
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode= "a"
+)
 
 
 def main(data,modelName,plotName,eta,epochs):
     df = pd.DataFrame(data)
+    logging.info(f"this is the raw data set :\n{df}\n")
     X, y = prepare_data(df)
     model = Perceptron(eta=eta, epochs=epochs)
     model.fit(X, y)
@@ -21,6 +33,13 @@ if __name__ == "__main__":
     }
     ETA = 0.3  # 0 and 1
     EPOCHS = 10
-    main(data=XOR,modelName = "xor.model",plotName="xor.png",eta=ETA,epochs=EPOCHS)
+    try:
+        logging.info(f">>>>>>>>>>>>>>>>>>>>Starting training for {gate}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        main(data=XOR,modelName = "xor.model",plotName="xor.png",eta=ETA,epochs=EPOCHS)
+        logging.info(f"<<<<<<<<<<<<<<<<<<<<<<{gate} Training completed <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n")
+    except Exception as e:
+        logging.exception(str(e))
+        raise e
+
 
 
